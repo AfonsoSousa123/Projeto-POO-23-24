5be89b02643e2617bbe123618f73430a0c979651
João Afonso Henriques de Sousa <44034661+AfonsoSousa123@users.noreply.github.com>
2023-10-01 00:36:25 +0100

    Estrutura do Projeto v1.0 (#12)
    * imagens trolas
    
    * imagens novas
    
    * Imagem do Ken de Lego
    
    * Mais imagens
    
    * Adicionadas as imagens para Ken e Barbie mais animacoes
    
    * pngs
    
    * pngs 2
    
    * Resize de imagens e reestruturacao das classes
    
    * tirar fundo
    
    * beach
    
    * Mundo mario style update
    
    ---------
    
    Co-authored-by: elisagv <128436366+elisagv@users.noreply.github.com>
    Co-authored-by: mimicarreira13 <mimicarreira13@hotmail.com>

--- Block.java
+++ Block.java
@@ -1,15 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Ken here.
+ * Write a description of class Block here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Ken extends Player
+public class Block extends Obstacle
 {
     /**
-     * Act - do whatever the Ken wants to do. This method is called whenever
+     * Act - do whatever the Block wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
